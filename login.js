65d808727c948d4fb772ac68b238e9a17af356e4
svb-meran <meran.svb@gmail.com>
2025-04-25 03:43:25 +0200

    Update login.js

--- login.js
+++ login.js
@@ -11,11 +11,11 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
     if (username === 'Jolicraft' && password === '#Fi5ccwg') {
         // Erfolgreiches Login -> Weiterleitung zum Dashboard und Admin-Status setzen
         localStorage.setItem('isAdmin', 'true');
-        window.location.href = 'dashboard.html';
+        window.location.href = 'übersicht.html';
     } else if (username === 'Marcel' && password === 'Marcel1234') {
         // Erfolgreiches Login für normale Benutzer -> Weiterleitung zum Dashboard
         localStorage.setItem('isAdmin', 'false');
-        window.location.href = 'dashboard.html';
+        window.location.href = 'übersicht.html';
     } else {
         // Fehlerhafte Anmeldung -> Fehlermeldung anzeigen
         document.getElementById('loginMessage').innerText = 'Falscher Benutzername oder Passwort!';
