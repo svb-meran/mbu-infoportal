d9a574dd7fa6f4e32c64922bfd3e571c051486b5
svb-meran <meran.svb@gmail.com>
2025-04-25 06:12:34 +0200

    Update login.js

--- login.js
+++ login.js
@@ -6,8 +6,8 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
 
     // Beispielnutzer – hier könntest du später echte Nutzer aus JSON laden
     const benutzer = {
-        "admin": { passwort: "adminpass", isAdmin: true },
-        "fahrer1": { passwort: "1234", isAdmin: false }
+        "Jolicraft": { passwort: "#Fi5ccwg", isAdmin: true },
+        "100nofunn100": { passwort: "112110", isAdmin: false }
     };
 
     if (benutzer[benutzername] && benutzer[benutzername].passwort === passwort) {
