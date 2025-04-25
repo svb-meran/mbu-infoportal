3b549f6ce0a6fe7c240a4f5cec155065e78a2bc3
svb-meran <meran.svb@gmail.com>
2025-04-25 13:28:50 +0200

    Update login.js

--- login.js
+++ login.js
@@ -8,7 +8,7 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
     const benutzer = {
         "Jolicraft": { passwort: "#Fi5ccwg", isAdmin: true },
         "100nofunn100": { passwort: "112110", isAdmin: false },
-        "merano1983": { passwort: "", isAdmin: false },
+        "merano1983": { passwort: "sicilia1983", isAdmin: false },
         "S0ul": { passwort: "", isAdmin: false },
         "Lukidrd": { passwort: "", isAdmin: false },
         "Bacon": { passwort: "", isAdmin: false },
